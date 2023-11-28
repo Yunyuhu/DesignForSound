2fcda917459047c004b9227758a0fbe64cade5ff
Yunyuhu <sandyfuoo68@gmail.com>
2023-11-28 21:58:24 +0800

    增加細部排版

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [micLevel, setMicLevel] = useState(0);
+  const [positions, setPositions] = useState([]);
 
   useEffect(() => {
     let mediaStream = null;
@@ -21,8 +22,7 @@ const App = () => {
       const updateMicLevel = () => {
         analyser.getByteFrequencyData(dataArray);
         const average = dataArray.reduce((acc, value) => acc + value, 0) / bufferLength;
-        setMicLevel(average/10);
-        // setMicLevel(average / 256); // 正規化到 0-1 之間
+        setMicLevel(average / 256); // 正規化到 0-1 之間
         requestAnimationFrame(updateMicLevel);
       };
 
@@ -49,25 +49,93 @@ const App = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const generatePositions = () => {
+      const newPositions = Array.from({ length: 20 }, () => ({
+        right: getRandomValue(0, 1000),
+        top: getRandomValue(0, 800),
+      }));
+      setPositions(newPositions);
+    };
+
+    generatePositions(); // 初始生成
+    const intervalId = setInterval(generatePositions, 300000); // 每 5 分鐘重新生成一次
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const ballSize = micLevel * 4000; // 調整球的大小
+
   return (
     <div>
-      <h1>聲音偵測器</h1>
-      <div>
-        <p>音量: {micLevel.toFixed(2)}</p>
-        {/* 在這裡可以使用音量的數值來畫一個動態的圖形 */}
-        <div
-          style={{
-            width: '100px',
-            height: '20px',
-            backgroundColor: 'blue',
-            transform: `scaleX(${micLevel})`,
-            transformOrigin: 'left',
-            transition: 'transform 0.2s ease-in-out',
-          }}
-        ></div>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center'
+      }}>
+        <h1 style={{
+          position: 'fixed',
+          marginTop: '50px'
+        }}>從現在開始，大聲說出你的想法！</h1>
+      </div>
+
+      <div style={{
+        position: 'relative',
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column'
+      }}>
+        <div>
+          {/* 使用球體 */}
+          <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: `${ballSize + 80}px`,
+                height: `${ballSize + 80}px`,
+                borderRadius: '50%',
+                backgroundColor: '#000000',
+                transition: 'width 0.2s ease-in-out, height 0.2s ease-in-out'
+              }}
+            >
+          {positions.map((position, index) => (
+            
+              <h1 key={index}
+              style={{
+                position: 'fixed',
+                color:'#FFFFFF',
+                ...position // 動態設定位置
+              }}>懷疑詞句</h1>
+              
+          ))}
+              
+              <div style={{flexDirection:'column'}}>
+                <h4 style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    color: "#FFFFFF"
+                  }}>speak loud</h4>
+                <p style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  color: "#FFFFFF"
+                }}>音量: {micLevel.toFixed(2)}</p>
+              </div>
+           </div>
+        </div>
       </div>
     </div>
   );
+};
+
+// 生成指定範圍內的隨機數
+const getRandomValue = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 export default App;
